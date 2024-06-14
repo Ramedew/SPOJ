1dcc807a2fa89e44aac759220dc7ca3f5454c999
Ramedew <118281423+Ramedew@users.noreply.github.com>
2024-06-15 00:05:29 +0200

    14.06.2024

--- SPOJ_stos.cpp
+++ SPOJ_stos.cpp
@@ -1,8 +1,41 @@
 #include <iostream>
 
-int stack[10];
-char znak;
 int main()
 {
-    
+    int stack[11],ile, liczba,rozmiar = 0;
+    char znak;
+    while(std::cin>>znak)
+    {
+        switch(znak)
+        {
+        case '+':
+            if (rozmiar >= 10)
+            {                
+                std::cin>>liczba;
+                std::cout << ":(" << std::endl;   
+            }
+            else
+            {
+                rozmiar++;
+                std::cin >> liczba;
+                stack[rozmiar] = liczba;
+                std::cout << ":)" << std::endl;
+            }
+            break;
+        case '-':
+            if (rozmiar >= 1)
+            {
+                std::cout << stack[rozmiar] << std::endl;
+                rozmiar--;
+            }
+            else
+            {
+                std::cout << ":(" << std::endl;
+            }
+            break;
+        default:
+        exit(0);
+        break;
+    }
+   }
 }
